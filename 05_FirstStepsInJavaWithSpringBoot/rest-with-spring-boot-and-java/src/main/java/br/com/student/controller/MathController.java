7ec96f1e0d150c63733eb34d881a79f11518f95f
Candeias-Vinicius <79974062+Candeias-Vinicius@users.noreply.github.com>
2022-11-02 16:59:52 -0300

    segundo commit

--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/student/controller/MathController.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/student/controller/MathController.java
@@ -1,4 +1,4 @@
-package br.com.student;
+package br.com.student.controller;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
